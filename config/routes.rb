4bd58aa19b794417703a6585909c6b9ecc351a0e
Erica Winne <erica.winne@powerhrg.com>
2021-09-08 14:20:13 -0400

    erica and rob pairing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/students", to: "students#index" #students method of index
+  get "/students/grades", to: "students#grades"
+  get "/students/highest-grade", to: "students#highest_grade" #routes go to: controller
+
 end
